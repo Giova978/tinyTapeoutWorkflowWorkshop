811b704e958a7a9b0a64b204386c1b1f589f30db
Giova <giovanni.hawas@gmail.com>
2025-09-04 17:02:07 -0400

    _

--- src/project.v
+++ src/project.v
@@ -5,12 +5,12 @@
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
+module tt_um_prueba (
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    input  wire [7:0] uio_in,   // IOs: Input path
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
